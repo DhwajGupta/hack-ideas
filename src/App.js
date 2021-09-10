feb24d79b83dbc1ae1f02648dce323fa8bfa50e7
Dhwaj Gupta <dhwajgupta27@gmail.com>
2021-09-10 23:05:34 +0530

    Add material ui, redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import Routes from './routes'
+import Loader from './components/Loader'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Suspense fallback={<Loader />}>
+      <Provider store={store}>
+        <Routes />
+        <ToastContainer />
+      </Provider>
+    </Suspense>
+  )
 }
 
-export default App;
+export default App
