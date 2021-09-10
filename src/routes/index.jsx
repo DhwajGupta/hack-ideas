8d49f721293591445d946b8f1b447e8424237495
Dhwaj Gupta <dhwajgupta27@gmail.com>
2021-09-10 23:45:23 +0530

    Add react router setup

---
+++ src/routes/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import CustomRoutes from './routes'
+
+const Routes = () => (
+  <Router>
+    <CustomRoutes />
+  </Router>
+)
+
+export default Routes
