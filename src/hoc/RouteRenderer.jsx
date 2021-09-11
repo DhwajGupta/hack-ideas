cbb8d07434e82f6565826802178d851d4ce98289
Dhwaj Gupta <dhwajgupta27@gmail.com>
2021-09-11 22:34:12 +0530

    Add hoc for private route

---
+++ src/hoc/RouteRenderer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom'
+import { EMPLOYEE_ID_SESSION_KEY } from '../constants'
+
+const RouteRenderer = (props) => {
+  const { private: isPrivateRoute } = props
+
+  const isUserLoggedIn = !!sessionStorage.getItem(EMPLOYEE_ID_SESSION_KEY)
+
+  if (isPrivateRoute && !isUserLoggedIn) {
+    return <Redirect to='/' />
+  }
+  return <Route exact {...props} />
+}
+
+export default RouteRenderer
+
